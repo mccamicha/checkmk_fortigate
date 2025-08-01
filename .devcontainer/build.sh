ca5b9685dd737debb92ee5580c83e12500169b26
MEIS <simon.meister86@gmail.com>
2025-08-01 10:05:46 +0200

    Try to fix action

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 NAME=$(python3 -c 'print(eval(open("package").read())["name"])')
 VERSION=$(python3 -c 'print(eval(open("package").read())["version"])')
 
-rm /omd/sites/cmk/var/check_mk/packages/${NAME} \
-   /omd/sites/cmk/var/check_mk/packages_local/${NAME}-*.mkp ||:
+#rm /omd/sites/cmk/var/check_mk/packages/${NAME} \
+#   /omd/sites/cmk/var/check_mk/packages_local/${NAME}-*.mkp ||:
 
 mkp -v package package 2>&1 | sed '/Installing$/Q' ||:
 
