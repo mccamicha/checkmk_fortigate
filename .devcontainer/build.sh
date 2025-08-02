edf4a67b9e592228bc542b0ad10983fd3f5372d5
Simon Meister <simon.meister@wagner.ch>
2025-08-02 11:39:20 +0000

    try to fix build

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -10,6 +10,7 @@ rm /omd/sites/cmk/var/check_mk/packages/${NAME} \
 
 mkp -v package package 2>&1 | sed '/Installing$/Q' ||:
 
+rm $NAME-$VERSION.mkp ||:
 cp /omd/sites/cmk/var/check_mk/packages_local/$NAME-$VERSION.mkp .
 
 mkp inspect $NAME-$VERSION.mkp
--- .devcontainer/symlink.sh
+++ .devcontainer/symlink.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-ln -sv $WORKSPACE/cmk_addons/ $OMD_ROOT/local/lib/python3/cmk_addons/
+ln -sv $WORKSPACE/cmk_addons $OMD_ROOT/local/lib/python3/cmk_addons/
 
 ln -sv $WORKSPACE/lib $OMD_ROOT/local/lib/python3/cmk
+
+ln -sv $WORKSPACE/cmk $OMD_ROOT/local/lib/python3/cmk
+
+ln -sv $WORKSPACE/plugins_legacy $OMD_ROOT/local/share/check_mk
 
 source /omd/sites/cmk/.profile && echo 'cmkadmin' | /omd/sites/cmk/bin/cmk-passwd -i cmkadmin
 
