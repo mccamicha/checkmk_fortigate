ba1d8a2032631e3dbe8592fa8685f0123a526cbb
Simon Meister <simon.meister@wagner.ch>
2025-08-02 10:25:21 +0000

    try to fix build

--- .devcontainer/symlink.sh
+++ .devcontainer/symlink.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-PKGNAME=$(python3 -c 'print(eval(open("package").read())["name"])')
-ln -sv $WORKSPACE/cmk_addons/plugins/$PKGNAME $OMD_ROOT/local/lib/python3/cmk_addons/plugins/$PKGNAME
+ln -sv $WORKSPACE/cmk_addons/ $OMD_ROOT/local/lib/python3/cmk_addons/
+
+ln -sv $WORKSPACE/lib $OMD_ROOT/local/lib/python3/cmk
 
 source /omd/sites/cmk/.profile && echo 'cmkadmin' | /omd/sites/cmk/bin/cmk-passwd -i cmkadmin
 
